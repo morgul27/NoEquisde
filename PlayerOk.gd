653a9f7da0b2dc4e1bba43c1e6ded968bfb61cad
morgul27 <joseminanca2000@gmail.com>
2024-09-22 13:28:56 +0200

    Gira camara
    Ahora si el script hace que gire la camara correctamente

--- PlayerOk.gd
+++ PlayerOk.gd
@@ -1,10 +1,40 @@
 extends CharacterBody3D
 
+@onready var MainCamera = get_node("Camera3D")
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var CameraRotation = Vector2(0,0)
+var MouseSensitivy = 0.001
 
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if event is InputEventMouseMotion:
+		var MouseEvent = event.relative *MouseSensitivy
+		print(event.relative)
+		CameraLook(MouseEvent)
+		
+func CameraLook(Movement: Vector2):
+	CameraRotation += Movement
+	CameraRotation.y = clamp(CameraRotation.y, -1.5, 1.2)
+	
+	transform.basis = Basis()
+	MainCamera.transform.basis = Basis()
+	
+	rotate_object_local(Vector3(0,1,0), -CameraRotation.x) #primera rotacion y
+	MainCamera.rotate_object_local(Vector3(1,0,0), -CameraRotation.y) #luego gira x
+	
+	
+
+#lo que sale predeterminado
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
